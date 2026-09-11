53b2ba46b362374ebbd289a6137d746cc6a9cf7a
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-09-11 23:01:30 +0530

    2459A

--- 2257B.cpp
+++ 2257B.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     int t;
     cin >> t;
+
     while (t--) {
         int n, m;
-        cin >> n >> m;  
-        
-        long long a1, b1;
-        // read all a, only need a[0]
-        cin >> a1;
-        for (int i = 1; i < n; i++) { long long x; cin >> x; }
-        
-        cin >> b1;
-        for (int i = 1; i < m; i++) { long long x; cin >> x; }
-        
-        // Bea goes first, so Bea wins if a[0] >= b[0]
-        cout << (a1 >= b1 ? 1 : 2) << "\n";
+        cin >> n >> m;
+
+        long long sumA = 0, sumB = 0;
+        long long x;
+
+        for (int i = 0; i < n; i++) {
+            cin >> x;
+            sumA += x;
+        }
+
+        for (int i = 0; i < m; i++) {
+            cin >> x;
+            sumB += x;
+        }
+
+        // Bea attacks first.
+        if (sumA >= sumB)
+            cout << 1 << '\n';
+        else
+            cout << 2 << '\n';
     }
+
     return 0;
 }
